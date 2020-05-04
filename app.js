57f8095f77e9e0819b600290f425b441b5f519ca
Marcel Melo <marcel.msmelo@gmail.com>
2020-05-04 16:49:07 -0300

    Adicionando biblioteca CORS

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const load = require('express-load');
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'mockups')));
 
 
 //==========================================================
