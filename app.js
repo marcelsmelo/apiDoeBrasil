05358f2e4b588d85acfc55c42c2b6ee252c7b2a0
Marcel Melo <marcel.msmelo@gmail.com>
2020-05-29 16:01:38 -0300

    Cluster on NodeJS

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ logger.level = 'error';
 let app = express();
 const load = require('express-load');
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
+// app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
+//     if (req.headers["x-forwarded-proto"] == "http") //Checa se o protocolo informado nos headers é HTTP 
+//         res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
+//     else //Se a requisição já é HTTPS 
+//         next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
 // });
 
 // view engine setup
